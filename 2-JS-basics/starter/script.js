68f96b8094bd130c528eeae8baee58ff4e4877b4
James <tytechortz@gmail.com>
2018-08-24 14:40:39 -0600

    updates file through boolean logic

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -125,6 +125,7 @@ x--;
 * CODING CHALLENGE 1
 */
 
+/*
 var massMark = 200;
 var hgtMark = 2;
 var massJohn = 110;
@@ -137,4 +138,62 @@ console.log(bmiMark, bmiJohn);
 var higherBMI = bmiMark > bmiJohn;
 
 console.log("Is Mark's BMI greater than John's?  " + higherBMI);
+*/
 
+/****************************************************
+* If / else statements
+*/
+
+/*
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully stay single! ');
+}
+    
+var isMarried = true;
+if (isMarried) {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully stay single! ');
+}
+
+var massMark = 200;
+var hgtMark = 2;
+var massJohn = 110;
+var hgtJohn = 1.68;
+
+bmiMark = massMark / (hgtMark * hgtMark);
+bmiJohn = massJohn / (hgtJohn * hgtJohn);
+console.log(bmiMark, bmiJohn);
+
+if (bmiMark > bmiJohn) {
+    console.log('Marks\' BMI is higher than John\'s.');
+    
+} else {
+    console.log('Johns\' BMI is higher than MArks\'s.');
+}
+
+//var higherBMI = bmiMark > bmiJohn;
+//console.log("Is Mark's BMI greater than John's?  " + higherBMI);
+*/
+
+/*************************************************************
+* Boolean logic
+*/
+
+var firstName = 'John'
+var age = 16;
+
+if (age < 13) {
+    console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20) {
+     console.log(firstName + ' is a teenager.');
+} else if (age >= 20 && age < 30) {
+    console.log(firstName + ' is a young man.');
+} else {
+    console.log(firstName + ' is a man.');
+}
