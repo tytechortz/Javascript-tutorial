98307dd5d065cfbc524c9d22b82c78ab8e04bbce
James <tytechortz@gmail.com>
2018-08-23 20:51:25 -0600

    starts variable mutationand type coercion

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,5 +1,32 @@
+/**********************************
+* Variables and data types
+*/
+
+/*
 var firstName = 'John';
 console.log(firstName);
 
 var lastName = 'Smith';
 var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// Variable naming rules
+var $3years = 3;
+var johnMark = 'John and Mark';
+*/
+/************************************
+* Variable mutation and type coercion
+*/
+
+var firstName = 'John'
+var age = 28;
+
+console.log(firstName + ' ' + age);
